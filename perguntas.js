586980805498751a292d57009bea20938136e94c
Yuri²² <00001091769357sp@al.educacao.sp.gov.br>
2024-11-29 15:53:41 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Doces',
+    'Qual é a origem do brigadeiro?',
+    'O brigadeiro é um doce brasileiro criado na década de 1940, originalmente como uma homenagem ao Brigadeiro Eduardo Gomes durante uma campanha presidencial.'
+)
+
+criaCartao(
+    'Doces',
+    'Como fazer caramelo perfeito?',
+    'Para um caramelo perfeito, cozinhe açúcar em fogo médio até derreter e caramelizar, adicionando creme de leite fresco e manteiga para textura e sabor.'
+)
+
+criaCartao(
+    'Doces',
+    'Qual a diferença entre brownie e bolo de chocolate?',
+    'O brownie é mais denso e úmido, enquanto o bolo de chocolate é mais leve e fofo, devido à maior quantidade de fermento.'
+)
+
+criaCartao(
+    'Doces',
+    'O que são macarons?',
+    'Macarons são doces franceses feitos com farinha de amêndoas, açúcar e claras de ovo, recheados com ganaches, geleias ou cremes.'
+)
+
+criaCartao(
+    'Doces',
+    'Quais os segredos para fazer uma cobertura de chocolate brilhante?',
+    'Use chocolate de boa qualidade, acrescente um pouco de glucose ou manteiga, e aplique a cobertura enquanto ainda está morna.'
+)
+criaCartao(
+    'Doces',
+    'Por que o pão de mel é tão famoso?',
+    'O pão de mel é famoso por sua combinação única de especiarias, mel e chocolate, oferecendo um sabor rico e uma textura macia que agrada a todos.'
+)
